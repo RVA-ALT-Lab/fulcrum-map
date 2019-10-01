36d61d2ef2b766101765da11947a70b2462dc0b0
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-10-01 13:08:49 -0400

    remove tile layers from all maps

--- 1885-gubernatorial-election-results/map.js
+++ 1885-gubernatorial-election-results/map.js
@@ -4,12 +4,6 @@ var MapUtilityClass = function ($) {
   this.initMap = function ( ) {
 
       var mymap = L.map('map').setView([37.5851, -79.0514], 7);
-      L.tileLayer('https://api.mapbox.com/styles/v1/jeffeverhart383/ck0qtifls19re1cmx0xkq0u19/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6IjIwNzVlOTA3ODI2MTY0MjM3OTgxMTJlODgzNjg5MzM4In0.QA1GsfWZccIB8u0FbhJmRg', {
-          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox.streets',
-          accessToken: 'pk.eyJ1IjoiamVmZmV2ZXJoYXJ0MzgzIiwiYSI6ImNqOXI2aDg5ejZhYncyd3M0bHd6cWYxc2oifQ.fzcb7maGkQhAxRZTotB4tg'
-      }).addTo(mymap);
       return mymap;
   }
 
